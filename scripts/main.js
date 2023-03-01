760ba1ff70c302de7be3ee873c1f64080eb4a998
May-ple-T <40469148+May-ple-T@users.noreply.github.com>
2023-03-02 02:09:29 +0900

    ヘッダーが重なるのを防止，その他諸々
    背景描写を変更，全体的に落ち着いたデザインに．

--- scripts/main.js
+++ scripts/main.js
@@ -22,7 +22,7 @@ var s_props = {
 $('#page-link a[href*="#"]').click(function () {
     var elmHash = $(this).attr('href'); //ページ内リンクのHTMLタグhrefから、リンクされているエリアidの値を取得
     console.log(elmHash);
-    var pos = $(elmHash).offset().top - 40;//idの上部の距離からHeaderの高さを引いた値を取得
+    var pos = $(elmHash).offset().top - 70;//idの上部の距離からHeaderの高さを引いた値を取得
     $('body,html').animate({ scrollTop: pos }, 500); //取得した位置にスクロール。500の数値が大きくなるほどゆっくりスクロール
     return false;
 });
